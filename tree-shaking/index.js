12e3589b9a63d16232c4e78d1acff11ea0d37549
dongleicode <mhwgodl@163.com>
2022-09-17 16:51:18 +0800

    tree-shaking

---
+++ tree-shaking/index.js
@@ -0,0 +1,2 @@
+import { foo } from './input.js'
+/*#__PURE__*/foo()  //这段代码不会产生副作用，你可以移除它
---
+++ tree-shaking/input.js
@@ -0,0 +1,8 @@
+//input.js
+export function foo(obj){
+    obj && obj.foo
+}
+
+export function bar(obj){
+    obj && obj.bar
+}
